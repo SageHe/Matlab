2ed8a755b1a1d553a8d3ac90bc837346aac74590
SageHe <sageherrin@gmail.com>
2020-10-25 22:42:11 -0600

    updating gnss hw 6 code

--- ASEN5090/HW6/compute_azelrange.m
+++ ASEN5090/HW6/compute_azelrange.m
@@ -2,5 +2,8 @@ function [az,el,range] = compute_azelrange(userECEF,satECEF)
 LOS_ENU = compute_LOS_ENU(userECEF,satECEF);
 az = atan2d(LOS_ENU(1),LOS_ENU(2));
 el = asind(LOS_ENU(3)/norm(LOS_ENU));
+if el < 0
+    el = (2*pi) - el;
+end
 range = norm(satECEF - userECEF);
 end
